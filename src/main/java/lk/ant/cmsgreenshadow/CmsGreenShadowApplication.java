b2c8fb0d7ceb66f5385b7627eeefe31f7df7c6df
Naveen Theekshana <ashannvn@gmail.com>
2024-11-04 22:12:12 +0530

    initial commit

---
+++ src/main/java/lk/ant/cmsgreenshadow/CmsGreenShadowApplication.java
@@ -0,0 +1,13 @@
+package lk.ant.cmsgreenshadow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsGreenShadowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsGreenShadowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ant/cmsgreenshadow/CmsGreenShadowApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ant.cmsgreenshadow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsGreenShadowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

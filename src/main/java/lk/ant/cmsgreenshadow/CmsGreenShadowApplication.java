10c0617ed4cc27978097a1c7d8a9c3e6fb12818e
Naveen Theekshana <ashannvn@gmail.com>
2024-12-02 20:45:02 +0530

    Mappings error resolved

--- src/main/java/lk/ant/cmsgreenshadow/CmsGreenShadowApplication.java
+++ src/main/java/lk/ant/cmsgreenshadow/CmsGreenShadowApplication.java
@@ -1,13 +1,20 @@
 package lk.ant.cmsgreenshadow;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CmsGreenShadowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CmsGreenShadowApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }

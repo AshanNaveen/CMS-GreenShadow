cf451bdf8449a9fe163912c7c8e899445d6057db
Naveen Theekshana <ashannvn@gmail.com>
2024-12-02 22:55:34 +0530

    added new method to field crop repository handling data by crop id

--- src/main/java/lk/ant/cmsgreenshadow/repository/FieldCropRepository.java
+++ src/main/java/lk/ant/cmsgreenshadow/repository/FieldCropRepository.java
@@ -3,10 +3,14 @@ package lk.ant.cmsgreenshadow.repository;
 import lk.ant.cmsgreenshadow.entity.FieldCropEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author Naveen Theekshana
  * @date 12/1/2024
  * @project CMSGreenShadow
  */
 public interface FieldCropRepository extends JpaRepository<FieldCropEntity,String> {
+    void deleteByCrop_CropId(String cropCropId);
+    List<FieldCropEntity> findByCrop_CropId(String cropCropId);
 }

f2e7c2aad29c8af0e372370cad89689e8e5f436a
Naveen Theekshana <ashannvn@gmail.com>
2024-12-03 20:32:02 +0530

    added new method to user repository handling data by email

--- src/main/java/lk/ant/cmsgreenshadow/repository/UserRepository.java
+++ src/main/java/lk/ant/cmsgreenshadow/repository/UserRepository.java
@@ -4,6 +4,9 @@ import lk.ant.cmsgreenshadow.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 /**
  * @author Naveen Theekshana
  * @date 11/21/2024
@@ -11,4 +14,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,String> {
+    Optional<UserEntity> findByEmail(String email);
 }

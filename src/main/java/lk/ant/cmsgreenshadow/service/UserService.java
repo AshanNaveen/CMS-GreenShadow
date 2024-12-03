50c02772ff72ba36a61d84ce2c856ce384450870
Naveen Theekshana <ashannvn@gmail.com>
2024-12-03 20:32:40 +0530

    user service implemented

--- src/main/java/lk/ant/cmsgreenshadow/service/UserService.java
+++ src/main/java/lk/ant/cmsgreenshadow/service/UserService.java
@@ -1,12 +1,14 @@
 package lk.ant.cmsgreenshadow.service;
 
 import lk.ant.cmsgreenshadow.dto.UserDto;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Naveen Theekshana
  * @date 11/24/2024
  * @project CMSGreenShadow
  */
+@Service
 public interface UserService {
     void saveUser(UserDto user);
     void updateUser(String email, UserDto user);
---
+++ src/main/java/lk/ant/cmsgreenshadow/service/impl/UserServiceIMPL.java
@@ -0,0 +1,71 @@
+package lk.ant.cmsgreenshadow.service.impl;
+
+import lk.ant.cmsgreenshadow.dto.UserDto;
+import lk.ant.cmsgreenshadow.entity.StaffEntity;
+import lk.ant.cmsgreenshadow.entity.UserEntity;
+import lk.ant.cmsgreenshadow.exception.DataPersistFailedException;
+import lk.ant.cmsgreenshadow.exception.NotFoundException;
+import lk.ant.cmsgreenshadow.repository.StaffRepository;
+import lk.ant.cmsgreenshadow.repository.UserRepository;
+import lk.ant.cmsgreenshadow.service.UserService;
+import lk.ant.cmsgreenshadow.util.MappingUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * @author Naveen Theekshana
+ * @date 11/24/2024
+ * @project CMSGreenShadow
+ */
+@Service
+public class UserServiceIMPL implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private StaffRepository staffRepository;
+    @Autowired
+    private MappingUtil mappingUtil;
+
+    @Override
+    public void saveUser(UserDto user) {
+        user.setRole(UserDto.Role.valueOf(getUserRole(user.getEmail()).name()));
+        UserEntity savedUser = userRepository.save(mappingUtil.toUserEntity(user));
+        if (savedUser != null) {
+            System.out.println("User saved successfully");
+        } else {
+            throw new DataPersistFailedException("User save unsuccessful");
+        }
+    }
+
+    @Override
+    public void updateUser(String email, UserDto user) {
+        Optional<UserEntity> tmpUserEntity = userRepository.findByEmail(email);
+        if (tmpUserEntity.isPresent()){
+            UserEntity userEntity = mappingUtil.toUserEntity(user);
+            tmpUserEntity.get().setPassword(userEntity.getPassword());
+            System.out.println("User password updated successfully: " + tmpUserEntity.get());
+        } else {
+            throw new NotFoundException("User not found with email: " + email);
+        }
+    }
+
+    @Override
+    public boolean searchUser(String email) {
+        Optional<StaffEntity> user = staffRepository.findByEmail(email);
+        return user.isPresent();
+    }
+
+    @Override
+    public boolean deleteUser(String email) {
+        return false;
+    }
+
+    private StaffEntity.Role getUserRole(String email) {
+        return staffRepository.findByEmail(email)
+                .map(StaffEntity::getRole)
+                .orElseThrow(() ->
+                        new NotFoundException("User not found for email: " + email));
+    }
+}

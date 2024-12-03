412407058d59fbcae5135a526ecf9139e9a577ff
Naveen Theekshana <ashannvn@gmail.com>
2024-12-03 20:31:35 +0530

    added new method to staff repository handling data by email

--- src/main/java/lk/ant/cmsgreenshadow/repository/StaffRepository.java
+++ src/main/java/lk/ant/cmsgreenshadow/repository/StaffRepository.java
@@ -4,6 +4,9 @@ import lk.ant.cmsgreenshadow.entity.StaffEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 /**
  * @author Naveen Theekshana
  * @date 11/21/2024
@@ -11,4 +14,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface StaffRepository extends JpaRepository<StaffEntity,String> {
+    Optional<StaffEntity> findByEmail(String email);
 }

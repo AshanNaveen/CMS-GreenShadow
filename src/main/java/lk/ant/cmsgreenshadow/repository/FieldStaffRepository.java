287f35bbdf406a854dd042da8d4448c400143fbb
Naveen Theekshana <ashannvn@gmail.com>
2024-12-03 19:38:26 +0530

    added new method to field staff repository handling data by field id

--- src/main/java/lk/ant/cmsgreenshadow/repository/FieldStaffRepository.java
+++ src/main/java/lk/ant/cmsgreenshadow/repository/FieldStaffRepository.java
@@ -4,6 +4,8 @@ import lk.ant.cmsgreenshadow.entity.FieldStaffEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author Naveen Theekshana
  * @date 12/1/2024
@@ -11,4 +13,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface FieldStaffRepository extends JpaRepository<FieldStaffEntity,String> {
+    void deleteByField_FieldId(String fieldId);
+    List<FieldStaffEntity> findByField_FieldId(String fieldId);
 }
